a6cc428e9b05a0c82c26a5018d7e4770f9afa1d2
Badreddine Ibril <badreddineibril@gmail.com>
2024-06-04 22:36:54 +0100

    COD-01: Add "Testimonial section

---
+++ src/views/pages/home/partials/testimonial/Testimonial.tsx
@@ -0,0 +1,38 @@
+import { Reviews } from "./helpers";
+
+export default function Testimonial() {
+  return (
+    <section id="testimonial" className="testimonial-container">
+      <div className="headlines section-head">
+        <div className="title">
+          <h5>
+            <span className="material-symbols-outlined">local_activity</span>
+            Testimonial
+          </h5>
+          <h1>Honest Reviews from Devs Like You</h1>
+        </div>
+      </div>
+      <div className="testimonial-box">
+        {
+          Reviews.map((item, i) => (
+            <div className="review-card" key={i}>
+              <div className="head">
+                <div className="owner">
+                  <img src={item.img} alt={item.name} className="picture" />
+                  <div className="info">
+                    <h4>{item.name}</h4>
+                    <span>@{item.username}</span>
+                  </div>
+                </div>
+              </div>
+              <div className="review-content">
+                <article>{item.review}</article>
+                <span className="date">May 24, 2024</span>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </section>
+  );
+}
